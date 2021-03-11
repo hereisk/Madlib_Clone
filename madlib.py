0e4131383aff0fb80ff284d8839535c346633ca9
hereisk <63280034+hereisk@users.noreply.github.com>
2021-03-10 20:22:10 -0500

    Update madlib.py

--- madlib.py
+++ madlib.py
@@ -80,9 +80,10 @@ ZOMBIE_PICNIC_USER_INPUT = [
 ]
 
 
-def choose_text():
+
+def choose_text(): #this function allows to the user to choose from three given texts
   while(True):
-    try:
+    try: #if the selection is valid the relevant text is selected and the code exits the loop
       print("Which story would you like to choose?\n1. Zombie Picnic.\n2. Is There a Monster in My Room?\n3. My Best"
             "Friend Is a Ghost!")
       selection = int(input("Please choose the number of the story: "))
@@ -98,21 +99,21 @@ def choose_text():
           text = GHOST
           user_input = GHOST_USER_INPUT
           break
-      elif selection == 0:
+      elif selection == 0: #if the user chooses to close the program they need to select 0
           print("Thanks for playing!")
           _exit(0)
-    except:
+    except: #this is to deal with invalid selection
           print("\nPlease choose a valid selection or enter 0 to exit\n")
   return text, user_input
 
 
-def say_it(text):
+def say_it(text): #the function is to create an audio file using gTTS module. The file is saved and played locally
     tts = gTTS(text=text, lang='en')
     tts.save('tts.wav')
     subprocess.call(["afplay", "tts.wav"])
 
 
-def main():
+def main(): #the main function to operate the program
     while True:
         selected_game = choose_text()
         text, user_input = selected_game
